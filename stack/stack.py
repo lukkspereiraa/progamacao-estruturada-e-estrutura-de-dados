5420890a75acb6ef7fc852f60f437e3cad198f8e
lukkspereiraa <monkeydlukks@gmail.com>
2024-10-01 14:12:14 -0300

    fazendo codigo de stack

--- stack/stack.py
+++ stack/stack.py
@@ -2,7 +2,29 @@ from lista_duplamemte_encadiada import DoublyLinkedList
 
 class Stack:
     def __init__(self) -> None:
-        self.__lista = DoublyLinkedList
+        self.__lista = DoublyLinkedList()
+    
+    def push(self, data):
+        self.__lista.append(data)
+    
+    def is_empty(self):
+        return self.__lista.size() == 0
+    
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("A pilha esta vazia")
+        
+        top = self.__lista.tail.data
+        self.__lista.remove_at(self.__lista.size() - 1)
+        return top
+    
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("A pilha esta vazia")
+        return self.__lista.tail.data
+    
+    def print_stack(self):
+        self.__lista.print_backward()
 
-pilha = Stack()
-pilha.lista.append(10)
+        
+#testes
